7d86f2c47576d3e5d8f060a2642eb575e84d2d82
LittleC <i@ltlec.cn>
2020-08-07 00:13:19 +0800

    :sparkles: je能build了

--- online_builder.py
+++ online_builder.py
@@ -1,16 +1,18 @@
 import importlib
-from flask import Flask, render_template, request, jsonify, send_file, send_from_directory, json, make_response
-import os
-import time
 import json
-from threading import Lock
+import os
 import subprocess
+import time
+from threading import Lock
+
+from flask import Flask, render_template, request, jsonify, send_from_directory, json, make_response
 
 app = Flask(__name__, template_folder='./views/',
             static_folder="static", static_url_path="/static")
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 nt = 0
 lock = Lock()
+
 
 # TODO: separate java sections and bedrock sections
 # TODO: get currently existent modules from both submodule
@@ -34,7 +36,8 @@ def generate_website():
     return render_template("index.html", mods=mods, enmods=enmods, language=language_modules, resource=resource_modules,
                            be_resource=be_builder.module_checker().get_module_list(),
                            be_manifests=be_builder.module_checker().get_manifests(),
-                           header_existence=header_existence, notice_existence=notice_existence, footer_existence=footer_existence, manifests=manifests)
+                           header_existence=header_existence, notice_existence=notice_existence,
+                           footer_existence=footer_existence, manifests=manifests)
 
 
 @app.route('/ajax', methods=['POST'])
@@ -46,22 +49,22 @@ def ajax():
         logs = ""
         if nt + 60 <= time.time():
             nt = time.time()
-            p = subprocess.Popen(["git", "pull", "origin", "master", "--recursive"],
+            p = subprocess.Popen(["git", "pull", "origin", "master", "--recurse-submodules"],
                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
             p.wait()
             logs += str(p.communicate()[0], 'utf-8', 'ignore')
         else:
             logs += 'Skipping the repository update because there\'s an available cache within 60 seconds.\n'
         # TODO: use importlib to introduce backend builder
-        '''
-        builder = build.builder()
-        builder.set_args(recv_data)
-        builder.build()
-        logs += builder.get_logs()
-        message = {"code": 200, "argument": recv_data,
-                   "logs": logs, "filename": builder.get_filename()}
+        message = {}
+        if not recv_data['_be']:
+            builder = importlib.import_module('meme-pack-java.build').builder()
+            builder.set_args(recv_data)
+            builder.build()
+            logs += builder.get_logs()
+            message = {"code": 200, "argument": recv_data,
+                       "logs": logs, "filename": builder.get_filename()}
         print(recv_data)
-        '''
     finally:
         lock.release()
     return json.dumps(message)
