1e57f2e8b81c8a537cd357e809cb912cce37c453
LittleC <i@ltlec.cn>
2020-10-24 17:16:13 +0800

    :sparkles: 修改一些构建设置

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,21 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 require('dotenv').config()
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? `${process.env.FE_DOMAIN}${process.env.PROJ_NAME}` : '/',
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production' ? `${process.env.FE_DOMAIN}${process.env.PROJ_NAME}` : './',
   "transpileDependencies": [
     "vuetify"
   ],
-
+  configureWebpack: config => {
+    if (process.env.ANALYZER) {
+      config.plugins.push(new BundleAnalyzerPlugin())
+    }
+    return {
+      externals: {
+        vue: 'Vue'
+      }
+    }
+  },
   pluginOptions: {
     i18n: {
       locale: 'zh-hans',
@@ -12,5 +23,8 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: false
     }
+  },
+  pwa: {
+    name: '梗体中文'
   }
 }
