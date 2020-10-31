ef11dd31873d0a1a704aea0c0d22948647917fa3
Dianliang <dianliang233@gmail.com>
2020-10-31 08:05:24 +0800

    ➖ remove @vue/cli-plugin-pwa

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -23,15 +23,5 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: false
     }
-  },
-  pwa: {
-    name: '梗体中文',
-    iconPaths: {
-      favicon32: './favicon-32x32.png',
-      favicon16: './favicon-16x16.png',
-      appleTouchIcon: './apple-touch-icon-152x152.png'
-    },
-    themeColor: '#FFFFFF',
-    appleMobileWebAppCapable: 'yes'
   }
 }
