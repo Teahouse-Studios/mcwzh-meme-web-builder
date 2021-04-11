710d943d6130e22d0266d100098c61238d15faa3
Dianliang233 <dianliang233@gmail.com>
2021-04-11 16:47:37 +0800

    👥 金主爸爸

--- src/App.vue
+++ src/App.vue
@@ -304,6 +304,56 @@
             </v-card>
           </v-col>
         </v-row>
+                <v-row class="mb-3">
+          <v-col col="12" md="6">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <div class="overline mb-4">{{ $t("sponsor.overline") }}</div>
+                  <v-list-item-title class="headline mb-1">{{ $t("sponsor.kkg.headline") }}</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{ $t("sponsor.kkg.subtitle") }}
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar color="grey" size="80" tile>
+                  <v-img src="https://attachment.mcbbs.net/uc_server/data/avatar/000/01/02/40_avatar_big.jpg"></v-img>
+                </v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn href="https://www.mcbbs.net/?10240" rel="noopener noreferrer" text>{{
+                    $t("sponsor.kkg.mcbbs")
+                  }}
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+                    <v-col col="12" md="6">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <div class="overline mb-4">{{ $t("sponsor.overline") }}</div>
+                  <v-list-item-title class="headline mb-1">{{ $t("sponsor.liu.headline") }}</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{ $t("sponsor.liu.subtitle") }}
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar color="transparent" size="80" tile>
+                  <v-img src="https://avatars.githubusercontent.com/u/65127014"></v-img>
+                </v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn href="https://www.mcbbs.net/?2444378" rel="noopener noreferrer" text>{{
+                    $t("sponsor.liu.website")
+                  }}
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
         <div :style="$vuetify.theme.dark !== true ? 'color: rgba(0,0,0,.6)' : 'color: rgba(256,256,256,.7)'"
              class="text-body-2 mb-3 pa-1">
           {{ $t("sponsor.disclaimer") }}
@@ -347,6 +397,7 @@
       </template>
     </v-snackbar>
     <v-dialog
+      persistent
       v-model="dialogFetchListFailed"
       width="500"
     >
@@ -366,7 +417,7 @@
           </v-btn>
           <v-btn
             text
-            @click="open(links.web_builder + '/issues/new/choose')"
+            @click="dialogFetchListFailed = false, $refs.help.dialog = true"
           >
             {{ $t("dialog.fetchListFailed.feedback") }}
           </v-btn>
