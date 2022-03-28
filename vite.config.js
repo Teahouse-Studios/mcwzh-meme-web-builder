4b18c309d13b180861eb992d554dcf2e8356c368
Dianliang233 <dianliang233@gmail.com>
2022-03-28 10:07:19 +0800

    Upgrade to vite

---
+++ vite.config.js
@@ -0,0 +1,77 @@
+import legacyPlugin from '@vitejs/plugin-legacy'
+import { defineConfig } from 'vite'
+import * as path from 'path'
+import { createVuePlugin } from 'vite-plugin-vue2'
+
+// @see https://cn.vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  let rollupOptions = {}
+
+  let optimizeDeps = {}
+
+  let alias = {
+    '.git': path.resolve(__dirname, './.git'),
+    '.github': path.resolve(__dirname, './.github'),
+    '.vscode': path.resolve(__dirname, './.vscode'),
+    dist: path.resolve(__dirname, './dist'),
+    node_modules: path.resolve(__dirname, './node_modules'),
+    public: path.resolve(__dirname, './public'),
+    src: path.resolve(__dirname, './src'),
+    '@': path.resolve(__dirname, '.src'),
+    vue$: 'vue/dist/vue.runtime.esm.js',
+  }
+
+  let proxy = {}
+
+  // todo 替换为原有变量
+  let define = {
+    'process.env.NODE_ENV':
+      command === 'serve' ? '"development"' : '"production"',
+  }
+
+  let esbuild = {}
+
+  return {
+    base: './', // index.html文件所在位置
+    root: './', // js导入的资源路径，src
+    resolve: {
+      alias,
+    },
+    define: define,
+    server: {
+      // 代理
+      proxy,
+    },
+    build: {
+      target: 'es2015',
+      minify: 'terser', // 是否进行压缩,boolean | 'terser' | 'esbuild',默认使用terser
+      manifest: false, // 是否产出maifest.json
+      sourcemap: false, // 是否产出soucemap.json
+      outDir: 'dist', // 产出目录
+      rollupOptions,
+    },
+    esbuild,
+    optimizeDeps,
+    plugins: [
+      createVuePlugin(),
+      legacyPlugin({
+        targets: [
+          'Android > 39',
+          'Chrome >= 60',
+          'Safari >= 10.1',
+          'iOS >= 10.3',
+          'Firefox >= 54',
+          'Edge >= 15',
+        ],
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        less: {
+          // 支持内联 JavaScript
+          javascriptEnabled: true,
+        },
+      },
+    },
+  }
+})
